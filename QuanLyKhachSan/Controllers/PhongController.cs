6bf2c33b791ef78c26af86a1c546404d7488ca9c
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-09-24 18:52:37 +0700

    themphong

---
+++ QuanLyKhachSan/Controllers/PhongController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyKhachSan.Controllers
+{
+    public class PhongController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
