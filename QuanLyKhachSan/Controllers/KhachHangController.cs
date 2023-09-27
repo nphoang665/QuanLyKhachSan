71bc118e1371a8b40bc6840e11a45c5be777f486
nphoang665 <phihoang665@gmail.com>
2023-09-27 18:12:13 +0700

    #lỗi tìm kh chưa fix

--- QuanLyKhachSan/Controllers/KhachHangController.cs
+++ QuanLyKhachSan/Controllers/KhachHangController.cs
@@ -92,7 +92,16 @@ namespace QuanLyKhachSan.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
-     
+
+         [HttpGet]
+        public async Task<IActionResult> Search(string searchText)
+        {
+            var khachHangs = await _db.KhachHangs
+                .Where(kh => kh.TenKhachHang.Contains(searchText)) // Thay ".TenKhachHang" bằng thuộc tính bạn muốn tìm kiếm
+                .ToListAsync();
+
+            return PartialView("_KhachHangPartial", khachHangs); // Trả về một PartialView chứa kết quả tìm kiếm
+        }
 
     }
 }
