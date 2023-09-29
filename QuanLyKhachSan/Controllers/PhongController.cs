4c7b0a40d3061037c6af751d9d8716f44167b8c0
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-09-29 18:49:43 +0700

    js tính toán

--- QuanLyKhachSan/Controllers/PhongController.cs
+++ QuanLyKhachSan/Controllers/PhongController.cs
@@ -8,5 +8,6 @@ namespace QuanLyKhachSan.Controllers
         {
             return View();
         }
+      
     }
 }
--- QuanLyKhachSan/wwwroot/js/phong.js
+++ QuanLyKhachSan/wwwroot/js/phong.js
@@ -12,3 +12,39 @@
         overlay.style.display = 'none';
     });
 });
+var checkInInput = document.querySelector('input[name="NgayNhan"]');
+var checkOutInput = document.querySelector('input[name="NgayTra"]');
+
+function updateDuration() {
+
+    var kiemtra = document.getElementsByClassName('combo_Gio_Ngay');
+    if (kiemtra.value == 'Gio') {
+        var checkInDate = new Date(checkInInput.value);
+        var checkOutDate = new Date(checkOutInput.value);
+
+        var duration = (checkOutDate - checkInDate) / 1000 / 60 / 60;
+        var thanhtien = duration * 10000;
+
+        document.querySelector('.duration').textContent = duration + ' giờ';
+        document.querySelector('.thanhtien').textContent = thanhtien;
+        document.querySelector('.tien_khachhang_tra').textContent = thanhtien;
+    }
+    else {
+        var checkInDate = new Date(checkInInput.value);
+        var checkOutDate = new Date(checkOutInput.value);
+
+        var duration = (checkOutDate - checkInDate) / 1000 / 60 / 60/24;
+        var thanhtien = duration * 10000;
+
+        document.querySelector('.duration').textContent = duration + ' ngày';
+        document.querySelector('.thanhtien').textContent = thanhtien;
+        document.querySelector('.tien_khachhang_tra').textContent = thanhtien;
+    }
+   
+
+   
+
+}
+
+checkInInput.addEventListener('change', updateDuration);
+checkOutInput.addEventListener('change', updateDuration);
