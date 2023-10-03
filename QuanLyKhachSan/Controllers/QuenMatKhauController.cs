0dfa3a580721b075bafc5286707a1e52726b9970
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-10-03 09:13:34 +0700

    ok

---
+++ QuanLyKhachSan/Controllers/QuenMatKhauController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Net;
+using System.Net.Mail;
+
+namespace QuanLyKhachSan.Controllers
+{
+    public class QuenMatKhauController : Controller
+    {
+    
+    
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> GuiMail(string Email, string ConfirmId)
+        //{
+          
+        //}
+    }
+}
--- QuanLyKhachSan/Program.cs
+++ QuanLyKhachSan/Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+
 using QuanLyKhachSan.DataAcess.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+// Add services to the container.\
+
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(
