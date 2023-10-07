0580717001d78e06b35c1154e522c949903c2c6b
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-10-07 16:44:20 +0700

    xong quenmk

--- QuanLyKhachSan/Controllers/QuenMatKhauController.cs
+++ QuanLyKhachSan/Controllers/QuenMatKhauController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using QuanLyKhachSan.DataAcess.Data;
 using System.Net;
 using System.Net.Mail;
 
@@ -7,43 +8,79 @@ namespace QuanLyKhachSan.Controllers
 {
     public class QuenMatKhauController : Controller
     {
-
-
+        private readonly ApplicationDbContext _db;
+        public QuenMatKhauController(ApplicationDbContext db)
+        {
+            _db= db;
+        }
+        public static string MaXacNhan;
+        public static string Gmail;
         public IActionResult Index()
         {
             return View();
-        }
-
+        } 
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> GuiMail(string Email, string ConfirmId)
+       
+        public async Task<IActionResult> GuiMail(string Email)
         {
-			string from, to, pass, content;
-			from = "khachsanasap@gmail.com";
-			to = Email;
-			pass = "ulwg gvjl vqmb iwya";
-			content = ConfirmId;
+            Gmail = Email;
+            Random rnd = new Random();
+            MaXacNhan = rnd.Next().ToString();
+            string from, to, pass, content;
+            from = "khachsanasap@gmail.com";
+            to = Email;
+            pass = "ulwg gvjl vqmb iwya";
+            content = MaXacNhan;
 
-			// Create a new SmtpClient object.
-			SmtpClient smtp = new SmtpClient("smtp.gmail.com");
-			smtp.EnableSsl = true;
-			smtp.Port = 587;
-			smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-			smtp.Credentials = new NetworkCredential(from, pass);
+            // Create a new SmtpClient object.
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(from, pass);
 
-			// Create a new MailMessage object.
-			MailMessage mail = new MailMessage();
-			mail.To.Add(to);
-			mail.From = new MailAddress(from);
-			mail.Subject = "Test";
-			mail.Body = content;
+            // Create a new MailMessage object.
+            MailMessage mail = new MailMessage();
+            mail.To.Add(to);
+            mail.From = new MailAddress(from);
+            mail.Subject = "Test";
+            mail.Body = content;
 
-			// Send the email.
-			await smtp.SendMailAsync(mail);
+            // Send the email.
+            await smtp.SendMailAsync(mail);
 
-			// Return a success response.
-			return RedirectToAction("Index", "QuenMatKhau");
-		}
+            // Return a success response.
+            return Json(new { success = true, message = "Email đã được gửi thành công!" });
+        }
+        [HttpPost]
+        public IActionResult XacNhanMa(string ConfirmId)
+        {
+            if (ConfirmId== MaXacNhan)
+            {
+                return Json(new { success = true, message = "Mã xác nhận hợp lệ!" });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Mã xác nhận không hợp lệ!" });
 
+            }
+        }
+        [HttpPost]
+        public IActionResult DoiMatKhau(string NewPassword)
+        {
+            var tk = _db.TaiKhoan.FirstOrDefault(s => s.Email == Gmail);
+            if (tk != null)
+            {
+                tk.MatKhau = NewPassword;
+                _db.SaveChanges();          
+                return Json(new { success = true, message = "Mật khẩu đã được đặt lại thành công!" });
+
+            }
+            else
+            {
+                return Json(new { success = false, message = "Mật khẩu đã được đặt lại không thành công!" });
+
+            }
+        }
     }
 }
---
+++ QuanLyKhachSan/wwwroot/js/QuenMatKhau.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    // Handle the Send Email button click
+    $('#sendEmail').click(function () {
+        var email = $('#email').val();
+        $.post("/QuenMatKhau/GuiMail", { Email: email }, function (data) {
+            if (data.success) {
+                // Show the next step
+                $('#step1').hide();
+                $('#step2').show();
+            } else {
+                // Show an error message
+                $('#message').text(data.message);
+            }
+        });
+    });
+
+    // Handle the Confirm Code button click
+    $('#confirmCode').click(function () {
+        var confirmId = $('#confirmId').val();
+        $.post("/QuenMatKhau/XacNhanMa", { ConfirmId: confirmId }, function (data) {
+            if (data.success) {
+                // Show the next step
+                $('#step2').hide();
+                $('#step3').show();
+            } else {
+                // Show an error message
+                $('#message').text(data.message);
+            }
+        });
+    });
+
+    // Handle the Reset Password button click
+    $('#resetPassword').click(function () {
+        var newPassword = $('#newPassword').val();
+
+        $.post("/QuenMatKhau/DoiMatKhau", { NewPassword: newPassword }, function (data) {
+            if (data.success) {
+                // Show a success message
+                $('#message').text('Mật khẩu đã được đặt lại thành công!');
+            } else {
+                // Show an error message
+                $('#message').text(data.message);
+            }
+        });
+    });
+});
