e20db90644f49ca10e542aaa09c70895fb47266f
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-10-09 21:56:53 +0700

    suaphong

--- QuanLyKhachSan/Controllers/QuanLyPhongController.cs
+++ QuanLyKhachSan/Controllers/QuanLyPhongController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuanLyKhachSan.DataAcess.Data;
+using QuanLyKhachSan.Model;
 
 namespace QuanLyKhachSan.Controllers
 {
@@ -13,6 +14,24 @@ namespace QuanLyKhachSan.Controllers
         public async Task<IActionResult> Index()
 		{
 			var phong =_db.Phong.ToList();
+			return View(phong);
+		}
+		[HttpGet]
+		public IActionResult SuaPhong(string id)
+		{
+			var maphong = _db.Phong.Find(id);
+			return View(maphong);
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult SuaPhong(Room phong)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Phong.Update(phong);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
 			return View(phong);
 		}
 	}
