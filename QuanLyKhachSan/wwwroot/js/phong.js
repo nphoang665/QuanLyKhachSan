fa40743b5d99ac9f37873382eb17091b99f11e4a
nhutbmt1212 <138290179+nhutbmt1212@users.noreply.github.com>
2023-10-16 22:45:18 +0700

    fix thành công đặt phòng

--- QuanLyKhachSan/wwwroot/js/phong.js
+++ QuanLyKhachSan/wwwroot/js/phong.js
@@ -53,14 +53,59 @@
 var checkInInput = document.querySelector('input[name="NgayNhan"]');
 var checkOutInput = document.querySelector('input[name="NgayTra"]');
 
-function updateDuration() {
+//function updateDuration() {
 
+//    var kiemtra = document.querySelector('.combo_Gio_Ngay');
+
+//    if (kiemtra.value == 'Gio') {
+//        var checkInDate = new Date(checkInInput.value);
+//        var checkOutDate = new Date(checkOutInput.value);
+//        var giaphong = 50000;
+//        var duration = ((checkOutDate - checkInDate) / 1000 / 60 / 60).toFixed(2);
+//        // Giá phòng theo giờ là 100000
+//        var thanhtien = (duration * giaphong).toFixed(2);
+
+//        document.getElementById('dukien').value = duration + ' giờ';
+//        document.getElementById('thanhtien').value = thanhtien;
+//        document.querySelector('.tien_khachhang_tra').textContent = thanhtien;
+//        document.getElementById('labelDuKien').textContent = document.getElementById('dukien').value;
+//        document.getElementById('lableThanhTien').textContent = document.getElementById('thanhtien').value;
+//        document.getElementById('gia-phong').value = giaphong;
+//        document.getElementById('labelGiaPhong').textContent = document.getElementById('gia-phong').value;
+//    }
+
+//    else {
+//        var checkInDate = new Date(checkInInput.value);
+//        var checkOutDate = new Date(checkOutInput.value);
+//        var giaphong = 500000;
+//        var duration = ((checkOutDate - checkInDate) / 1000 / 60 / 60 / 24).toFixed(2);
+//        // Giá phòng theo ngày là 800000
+//        var thanhtien = (duration * giaphong).toFixed(2);
+
+//        document.getElementById('dukien').value = duration + ' ngày';
+//        document.getElementById('thanhtien').value = thanhtien;
+//        document.querySelector('.tien_khachhang_tra').textContent = thanhtien;
+//        document.getElementById('labelDuKien').textContent = document.getElementById('dukien').value;
+//        document.getElementById('lableThanhTien').textContent = document.getElementById('thanhtien').value;
+//        document.getElementById('gia-phong').value = giaphong;
+//        document.getElementById('labelGiaPhong').textContent = document.getElementById('gia-phong').value;
+//    }
+//}
+function updateDuration() {
+    var maPhong = document.getElementById('Ma_Phong').value;
     var kiemtra = document.querySelector('.combo_Gio_Ngay');
 
-    if (kiemtra.value == 'Gio') {
-        var checkInDate = new Date(checkInInput.value);
+    // Gọi API để lấy giá phòng
+    $.ajax({
+        url: '/Phong/GetRoomPrice',
+        type: 'GET',
+        data: { maPhong: maPhong },
+        success: function (room) {
+            var giaphong;
+            if (kiemtra.value == 'Gio') {
+                giaphong = room.giaTheoGio;
+                var checkInDate = new Date(checkInInput.value);
         var checkOutDate = new Date(checkOutInput.value);
-        var giaphong = 50000;
         var duration = ((checkOutDate - checkInDate) / 1000 / 60 / 60).toFixed(2);
         // Giá phòng theo giờ là 100000
         var thanhtien = (duration * giaphong).toFixed(2);
@@ -72,12 +117,10 @@ function updateDuration() {
         document.getElementById('lableThanhTien').textContent = document.getElementById('thanhtien').value;
         document.getElementById('gia-phong').value = giaphong;
         document.getElementById('labelGiaPhong').textContent = document.getElementById('gia-phong').value;
-    }
-
-    else {
-        var checkInDate = new Date(checkInInput.value);
+            } else {
+                giaphong = room.giaTheoNgay;
+                var checkInDate = new Date(checkInInput.value);
         var checkOutDate = new Date(checkOutInput.value);
-        var giaphong = 500000;
         var duration = ((checkOutDate - checkInDate) / 1000 / 60 / 60 / 24).toFixed(2);
         // Giá phòng theo ngày là 800000
         var thanhtien = (duration * giaphong).toFixed(2);
@@ -89,9 +132,13 @@ function updateDuration() {
         document.getElementById('lableThanhTien').textContent = document.getElementById('thanhtien').value;
         document.getElementById('gia-phong').value = giaphong;
         document.getElementById('labelGiaPhong').textContent = document.getElementById('gia-phong').value;
-    }
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
 }
-
 
 checkInInput.addEventListener('change', updateDuration);
 checkOutInput.addEventListener('change', updateDuration);
