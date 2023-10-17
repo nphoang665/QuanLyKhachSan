0dd8fe8ef97702a171512fa618cd9a3333398206
nphoang665 <phihoang665@gmail.com>
2023-10-17 12:32:41 +0700

    #FixLoiVat

--- QuanLyKhachSan/wwwroot/js/script.js
+++ QuanLyKhachSan/wwwroot/js/script.js
@@ -1,4 +1,4 @@
-const container = document.getElementById('container');
+﻿const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 const forgotPw = document.getElementById('Forgot_Password');
@@ -17,9 +17,9 @@ forgotPw.addEventListener('click', (e) => {
     e.preventDefault(); // prevent the default action
     container.classList.add("forgot");
     toggleLeft.innerHTML = `
-        <h1>Welcome Back!</h1>
-        <p>Enter your personal details and start journey with us</p>
-        <button class="hidden" id="new-login">Sign In</button>
+        <h1>Welcome A$AP ROYAL</h1>
+        <p>Nhập tài khoản & mật khẩu của bạn để sử dụng tất cả các tính năng của trang web</p>
+        <button class="hidden" id="new-login">ĐĂNG NHẬP</button>
     `;
 
     // Add a click event for the new "Sign In" button
@@ -27,9 +27,9 @@ forgotPw.addEventListener('click', (e) => {
     newLoginBtn.addEventListener('click', () => {
         container.classList.remove("forgot");
         toggleRight.innerHTML = `
-            <h1>Hello, Friend!</h1>
-            <p>Register with your personal details to use all of site features</p>
-            <button class="hidden" id="new-register">Sign Up</button>
+            <h1>Welcome A$AP ROYAL</h1>
+            <p>Đăng ký với thông tin cá nhân của bạn để sử dụng tất cả các tính năng của trang web</p>
+            <button class="hidden" id="new-register">ĐĂNG KÝ</button>
         `;
 
         // Add a click event for the new "Register" button
